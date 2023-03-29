620b7afa9e25467beb4334a48857a3a6400e90a8
igorpadua <igorpadua@tutanota.com>
2023-03-29 16:50:18 -0300

    test: Criado os testes com spock

--- backend/src/test/groovy/todolistgrails/TaskSpec.groovy
+++ backend/src/test/groovy/todolistgrails/TaskSpec.groovy
@@ -11,8 +11,68 @@ class TaskSpec extends Specification implements DomainUnitTest<Task> {
     def cleanup() {
     }
 
-    void "test something"() {
-        expect:"fix me"
-            true == false
+    def "Criar uma tarefa"() {
+        given:
+        Task task = new Task(nome: "Tarefa 1", descricao: "Descrição da tarefa 1", dataTermino: new Date(),
+                prioridade: 1, categoria: "Categoria 1", status: "A fazer")
+
+        when:
+        task.save flush: true
+
+        then:
+        task.id != null
+    }
+
+    def "Atualizar uma tarefa"() {
+        given:
+        Task task = new Task(nome: "Tarefa 1", descricao: "Descrição da tarefa 1", dataTermino: new Date(),
+                prioridade: 1, categoria: "Categoria 1", status: "A fazer")
+        task.save flush: true
+
+        when:
+        task.nome = "Tarefa 2"
+        task.save flush: true
+
+        then:
+        task.nome == "Tarefa 2"
+    }
+
+    def "Listar tarefas"() {
+        given:
+        Task task = new Task(nome: "Tarefa 1", descricao: "Descrição da tarefa 1", dataTermino: new Date(),
+                prioridade: 1, categoria: "Categoria 1", status: "A fazer")
+        task.save flush: true
+
+        when:
+        List<Task> tasks = Task.list()
+
+        then:
+        tasks.size() == 1
+    }
+
+    def "Pegar um tarefa"() {
+        given:
+        def task = new Task(nome: "Tarefa 1", descricao: "Descrição da tarefa 1", dataTermino: new Date(),
+                prioridade: 1, categoria: "Categoria 1", status: "A fazer")
+        task.save flush: true
+
+        when:
+        Task task2 = Task.get(task.id)
+
+        then:
+        task2.id == task.id
+    }
+
+    def "Remover uma tarefa"() {
+        given:
+        Task task = new Task(nome: "Tarefa 1", descricao: "Descrição da tarefa 1", dataTermino: new Date(),
+                prioridade: 1, categoria: "Categoria 1", status: "A fazer")
+        task.save flush: true
+
+        when:
+        task.delete flush: true
+
+        then:
+        Task.get(task.id) == null
     }
 }
